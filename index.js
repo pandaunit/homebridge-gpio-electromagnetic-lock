d995f08e9ee456eddf6ae366c1ce0918e2bb280f
Radoslaw Sporny <r.sporny@gmail.com>
2017-12-15 15:34:43 +0100

    first release

---
+++ index.js
@@ -0,0 +1,62 @@
+var _ = require('underscore');
+var rpio = require('rpio');
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+
+  homebridge.registerAccessory('homebridge-gpio-stateless-switch', 'StatelessSwitch', StatelessSwitchAccessory);
+}
+
+function StatelessSwitchAccessory(log, config) {
+  _.defaults(config, {activeLow: true});
+
+  this.log = log;
+  this.name = config['name'];
+  this.pin = config['pin'];
+  this.initialState = config['activeLow'] ? rpio.HIGH : rpio.LOW;
+  this.activeState = config['activeLow'] ? rpio.LOW : rpio.HIGH;
+
+  this.service = new Service.Switch(this.name);
+
+  this.infoService = new Service.AccessoryInformation();
+  this.infoService
+    .setCharacteristic(Characteristic.Manufacturer, 'Radoslaw Sporny')
+    .setCharacteristic(Characteristic.Model, 'RaspberryPi GPIO Stateless Switch')
+    .setCharacteristic(Characteristic.SerialNumber, 'Version 1.0.0');
+
+  // use gpio pin numbering
+  rpio.init({
+    mapping: 'gpio'
+  });
+  rpio.open(this.pin, rpio.OUTPUT, this.initialState);
+
+  this.service
+    .getCharacteristic(Characteristic.On)
+    .on("get", this.getStatus.bind(this))
+    .on('set', this.setStatus.bind(this));
+}
+
+StatelessSwitchAccessory.prototype.getStatus = function(callback) {
+  callback(null, false);
+}
+
+StatelessSwitchAccessory.prototype.setStatus = function(value, callback) {
+  if (!value) {
+    callback();
+    return true;
+  }
+  this.log('Setting switch on');
+  rpio.write(this.pin, this.activeState);
+  setTimeout(function() {
+    this.log('Setting switch off');
+    rpio.write(this.pin, this.initialState);
+    this.service.setCharacteristic(Characteristic.On, false);
+  }.bind(this), 700);
+  callback();
+}
+
+StatelessSwitchAccessory.prototype.getServices = function() {
+  return [this.infoService, this.service];
+}
